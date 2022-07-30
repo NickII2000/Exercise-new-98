51f790d885709889314ae60d2469bfc8826d8591
Nick <Nick_II2000@mail.ru>
2022-07-30 19:34:32 +0600

    19-th commit

--- js/script.js
+++ js/script.js
@@ -608,3 +608,21 @@ window.addEventListener('DOMContentLoaded', () => {
     getDynamicInformation('#weight');
     getDynamicInformation('#age');
 });
+
+'use strict';
+
+const person = {
+    name: 'Alex',
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(n) {
+        this.age = n;
+    },
+};
+
+console.log(person.userAge);
+console.log(person.userAge = 52);
